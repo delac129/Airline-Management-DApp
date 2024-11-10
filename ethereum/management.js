4a0cba97c734d72f0551c18e845b722bdaccb97c
Andres Hinojosa <108111237+Antech15@users.noreply.github.com>
2024-11-09 18:39:13 -0800

    fix

--- ethereum/management.js
+++ ethereum/management.js
@@ -2,7 +2,7 @@ import web3 from "./web3";
 import AirlineManagement from "./build/AirlineManagement.json";
 
 const instance = new web3.eth.Contract(
-  AirlineManagement,
+  AirlineManagement.abi,
   "0x822157A35DfFa3a1A6D8009909B10eDf6940B59E"
 );
 
