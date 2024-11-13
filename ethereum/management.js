0fb9ff0e36b7876faccb3902ba95668f7244ff0b
Andres Hinojosa <108111237+Antech15@users.noreply.github.com>
2024-11-13 15:50:25 -0800

    lol

--- ethereum/management.js
+++ ethereum/management.js
@@ -3,7 +3,7 @@ import AirlineManagement from "./build/AirlineManagement.json";
 
 const instance = new web3.eth.Contract(
   AirlineManagement.abi,
-  "0x822157A35DfFa3a1A6D8009909B10eDf6940B59E"
+  "0x7a975C320a7d5BbdB961FDB4c2BFd15869DD2155"
 );
 
 export default instance;
