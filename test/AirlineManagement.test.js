7605d59c0e84d47d604cdc006b7e46cfa72eebe1
Andres Hinojosa <108111237+Antech15@users.noreply.github.com>
2024-11-19 11:58:33 -0800

    finished test

--- test/AirlineManagement.test.js
+++ test/AirlineManagement.test.js
@@ -1,7 +1,9 @@
 const assert = require('assert');
 const ganache = require('ganache');
 const { Web3 } = require("web3");
-const web3 = new Web3(ganache.provider());
+const web3 = new Web3(ganache.provider({
+  gasLimit: 10000000000
+}));
 
 const compiledFactory = require('../ethereum/build/AirlineManagement.json'); // Updated path
 
@@ -9,6 +11,8 @@ let accounts;
 let airlineManagement;
 
 beforeEach(async () => {
+  ogLog = console.log;    //To reduce
+  console.log = () => {}; //info in termial
   accounts = await web3.eth.getAccounts();
 
   // Deploy the AirlineManagement contract with arguments
@@ -17,23 +21,33 @@ beforeEach(async () => {
       data: compiledFactory.evm.bytecode.object,
       arguments: ["Miami", "70"]
     })
-    .send({ from: accounts[0], gas: "8000000" });
-
+    .send({ from: accounts[0], gas: "10000000000" });
+    
+  console.log = ogLog; //To reduce info in terminal
   console.log("AirlineManagement contract deployed at: ", airlineManagement.options.address);
 });
 
 describe("AirlineManagement Contract", () => {
-  it("should deploy the contract", async () => {
+  it("The contract is deployed", () => {
     assert.ok(airlineManagement.options.address);
   });
 
-  it("should initialize with the correct destination and price", async () => {
-    const destinations = await airlineManagement.methods.destinations(0).call();
-    const price = await airlineManagement.methods.destinationPrices(0).call();
-
-    assert.strictEqual(destinations, "Miami");
-    assert.strictEqual(price, "70");
+  it("The CEO can create a new destination", async () => {
+    try {
+      await airlineManagement.methods.createDestination("New York", "100").send({from: accounts[0], gas: 9000000});
+      assert(true);
+    } catch (err) {
+      assert(false);
+    }
   });
-
-  // Add more tests as needed
+  it("A customer can book a flight", async () => {
+    let price = await airlineManagement.methods.getPrice("Miami", 4, 1, true).call();
+    try {
+      await airlineManagement.methods.bookFlight("Miami", 4, 1, true).send({from: accounts[1], value: price, gas: 9000000});
+    } catch (err) {
+      assert(false);
+    }
+    let booked = await airlineManagement.methods.getBookedFlights(accounts[1]).call();
+    assert(booked);
+  });
 });
